81bcaa9e35493f7f74ccbd60af0d7d710d757a6b
Alex Vogt <alexvogt18@gmail.com>
2021-04-28 16:09:38 -0700

    Functional accordion, working localStorage next

--- welcome.js
+++ welcome.js
@@ -1,44 +1,63 @@
 'use strict';
-
-const acc = document.getElementsByClassName("accordion");
-function slide(){
-    let i;
-    for (let i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            console.log('I was clicked!');
-            this.classList.toggle('active'); 
-            const panel = acc[i].nextElementSibling;
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            } 
+// ==================Accordion===========================
+const accordion = document.getElementsByClassName('contentBx');
+for (let i = 0; i < accordion.length; i++) {
+            accordion[i].addEventListener('click', function() {
+                console.log('I was clicked!');
+                this.classList.toggle('active');
+                let panel=this.nextElementSibling;
+                if(panel.style.display==='block'){
+                    panel.style.display='none';
+                }  else{
+                    panel.style.display='block';
+                }
         });
-    }
-}
-// =================Local Store Storage Code=======================
-const playeroneInput = document.getElementById("PoneName") 
-const playertwoInput = document.getElementById("PtwoName")
- 
+    };
+// =================Local Store Storage Code=======================  
 function storeplayerOne() {
+    const playeroneInput = document.getElementById("PoneName")
     // stringify the drinks 
-    console.log(`${playeroneInput}`);
+    console.log(playeroneInput);
     let stringifiedName = JSON.stringify(playeroneInput);
     console.log(stringifiedName);
     // place stringified drinks in storage
-    localStorage.setItem('Player 1', `${playeroneInput}`);
+    localStorage.setItem('Player 1', stringifiedName);
   }
 
 function storeplayerTwo(){
-    console.log(`${playertwoInput}`)
+    const playertwoInput = document.getElementById("PtwoName")
+    console.log(playertwoInput)
     let stringifiedName = JSON.stringify(playertwoInput);
     console.log(stringifiedName);
-    localStorage.setItem('Player 2', `${playertwoInput}`)
+    localStorage.setItem('Player 2', stringifiedName)
 }
+//===================Play Button================================
+function startPlaying(){
+    const playButton = document.getElementsByClassName('play');
+    let i;
+    for (let i = 0; i < playButton.length; i++) {
+        playButton[i].addEventListener('click', function () {
+        storeplayerOne();
+        storeplayerTwo();
+        });
+    };
+};
 //=================Local Get Storage Code==========================
+function getplayerOne(){
+    let retrievedName = localStorage.getItem('Player 1');
+    console.log(retrievedName, "is Player 1's name")
+    if (retrievedName !== null) {
+        let parsedOrders = JSON.parse(retrievedName);
+        console.log(parsedOrders);
+    }
+}
 
-
-
-storeplayerOne();
-storeplayerTwo();
-slide();
+function getplayerTwo(){
+    let retrievedName = localStorage.getItem('Player 2');
+    console.log(retrievedName, "is Player 2's name")
+    if (retrievedName !== null) {
+        let parsedOrders = JSON.parse(retrievedName);
+        console.log(parsedOrders);
+    }
+}
+startPlaying();
