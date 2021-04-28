0f0bb102ae38fd1f54fa423c744ad2ee395e17eb
Alex Vogt <alexvogt18@gmail.com>
2021-04-27 18:23:33 -0700

    Fixed most accordion issues.

--- welcome.js
+++ welcome.js
@@ -1,7 +1,20 @@
 'use strict';
 
+const acc = document.getElementsByClassName("accordion");
 function slide(){
-
+    let i;
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", (e) =>{
+            console.log('I was clicked!');
+            this.classList.toggle('active'); 
+            const panel = acc[i].nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
+        });
+    }
 }
 // =================Local Storage Code=======================
 
